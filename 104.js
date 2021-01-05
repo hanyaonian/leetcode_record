3bb00d62f3ea9c63734d894d5df0da19a04bbc2f
MichaelHyn <hanyaonian@outlook.com>
2021-01-05 23:13:04 +0800

    1-5 @ 104

---
+++ 104.js
@@ -0,0 +1,32 @@
+/**
+ * 给定一个二叉树，找出其最大深度。
+
+	二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+
+	说明: 叶子节点是指没有子节点的节点。
+
+
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const finder = (root) => {
+    if (root === null) {
+        return 0;
+    }
+    return 1 + Math.max(finder(root.left), finder(root.right))
+}
+
+var maxDepth = function(root) {
+    if (root === null) {
+        return 0;
+    }
+    return finder(root);
+};
