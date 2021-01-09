6612fc3a505d37703197ec1e58b145ade026c641
MichaelHyn <hanyaonian@outlook.com>
2021-01-09 13:21:29 +0800

    1-9 @ 141

---
+++ 141.js
@@ -0,0 +1,38 @@
+/**
+ * 给定一个链表，判断链表中是否有环。
+ * 简单题。
+ * 
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    let nodeSet = new Set();
+    if (!head) {
+        return false
+    }
+    while(head) {
+        if (nodeSet.has(head)) {
+            return true;
+        }
+        nodeSet.add(head);
+        head = head.next;
+    }
+    return false;
+};
+
+//  进阶解法， 快慢指针，空间复杂度1
+var hasCycle = function(head) {
+    if (!head) {
+        return false
+	}
+	let fast = head, slow = head;
+	while (slow != fast) {
+		//有尽头
+        if (!fast || !fast.next) {
+            return false;
+		}
+		fast = fast.next.next;
+		slow = slow.next;
+    }
+    return true; // 有环，相遇了
+};
