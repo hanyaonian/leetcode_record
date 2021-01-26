ad1b5518f8a4ef86ce93335839de0cf86c33ab6b
MichaelHyn <hanyaonian@outlook.com>
2021-01-26 23:55:41 +0800

    1-26 @ 22

---
+++ 22*.js
@@ -0,0 +1,31 @@
+/**
+ * 数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
+	示例 1：
+
+	输入：n = 3
+	输出：["((()))","(()())","(())()","()(())","()()()"]
+	示例 2：
+
+	输入：n = 1
+	输出：["()"]
+ * 
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function(n) {
+    const ans = [], stack = [];
+    const dfs = (left, right, str) => {
+        if (left === 0 && right === 0) {
+            ans.push(str);
+            return;
+        }
+        if (left > 0) {
+            dfs(left - 1, right, str + '(')
+        }
+        if (right > left) {
+            dfs(left, right - 1, str + ')')
+        }
+    }
+    dfs(n, n, '');
+    return ans;
+};
