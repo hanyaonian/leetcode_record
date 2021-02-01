a5f369f8490070c52e3904c4ce57d651148204f1
Michael <hanyaonian@outlook.com>
2021-02-01 15:15:58 +0800

    2-1 @ 218
    2-1 @ 218

---
+++ 218.js
@@ -0,0 +1,13 @@
+/**
+
+    给定一个整数，编写一个函数来判断它是否是 2 的幂次方。
+    easy
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function(n) {
+    while (Number.isInteger(n / 2)) {
+        n = n / 2;
+    }
+    return n === 1;
+};
